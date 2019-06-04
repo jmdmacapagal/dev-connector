bd09e9a82961efb7d2aecfeb386a9386dc1275dc
jm macapagal <jmdmacapagal@gmail.com>
2019-06-05 04:42:13 +0800

    configured registration feature

--- config/db.js
+++ config/db.js
@@ -4,13 +4,13 @@ const config = require('config');
 const db = config.get('mongoURI');
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(db, { useNewUrlParser: true }, () => console.log('Mongodb connected'));
-  } catch (error) {
-    console.error(error.message);
-    // exit process with failure
-    process.exit(1);
-  }
+    try {
+        await mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true }, () => console.log('Mongodb connected'));
+    } catch (error) {
+        console.error(error.message);
+        // exit process with failure
+        process.exit(1);
+    }
 };
 
 module.exports = connectDB;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,21 +1,57 @@
 const router = require('express').Router();
-const { check, validationResult } = require('express-validator/check')
+const { check, validationResult } = require('express-validator/check');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const User = require('../../models/User');
 
 // @route   GET api/users
 // @desc    test route
 // @access  public
 
-router.post('/', [
-    check('name', 'Name is required').not().isEmpty(),
-    check('email', 'Please include a valid email.').isEmail(),
-    check('password', 'Please enter password with 6 or more characters.').isLength({ min: 6 })
-], (req, res) => {
-    const error = validationResult(req);
-    if (!error.isEmpty()) {
-        return res.status(400).json({ error: error.array() });
-    }
-    res.send('User Route');
-});
 
+router.post('/', [
+    check('name', 'Please enter your name.').not().isEmpty(), // field validation parameters
+    check('email', 'Please enter a valid email address.').isEmail(),
+    check('password', 'Please enter password with a minimum of 6 characters.')
+], async (req, res) => {
+    const error = validationResult(req); // input validation
+    if (!error.isEmpty()) {
+        return res.status(400).send({ error: error.array() });
+    }
+
+    const { name, email, password } = req.body; // variable deconstruction
+
+    try {
+        let user = await User.findOne({ email }); // check if email already exists
+        if (user) {
+            return res.status(400).send({ error: [{ msg: 'Email Already Exists.' }] });
+        }
+        // setup gravatar
+        const avatar = gravatar.url(email, {
+            s: '200',
+            r: 'pg',
+            d: 'mm'
+        });
+
+        // create new user instance
+        user = new User({
+            name,
+            email,
+            avatar,
+            password
+        });
+
+        // hash password
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+
+        // save user to db
+        await user.save();
+        res.send('User Registered');
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 
 module.exports = router;
