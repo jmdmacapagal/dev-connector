01bf129522bb072f6cc95a5978db6fd51824b46a
jm macapagal <jmdmacapagal@gmail.com>
2019-06-11 02:23:39 +0800

    added GET route to find all posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -33,4 +33,17 @@ router.post('/', [auth, [
     }
 });
 
+// @route   GET api/posts
+// @desc    get all posts
+// @access  private
+router.get('/', auth, async (req, res) => {
+    try {
+        const posts = await Post.find().sort({ date: -1 });
+        res.json(posts);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error.');
+    }
+});
+
 module.exports = router;
